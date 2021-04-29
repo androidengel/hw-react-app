474ec83a78f8ceae7f3883ae9bb2251de3fc7d32
Andrew <androidengel@gmail.com>
2021-04-29 11:39:46 -0500

    Connecting frontend to backend Configuring successful communication from the backend to display data on the frontend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./styles.css";
 import { useQuery, gql } from "@apollo/client";
 
-const ALL_RECIPES = gql`
-  query GetAllRecipes {
-    recipe(id: 1) {
+const ALL_LINKS = gql`
+  query GetAllLinks {
+    allLinks {
       id
-      ingredients
+      url
+      slug
     }
   }
 `;
 
 export default function App() {
-  const { loading, err, data } = useQuery(ALL_RECIPES);
+  const { loading, err, data } = useQuery(ALL_LINKS);
   console.log(JSON.stringify(data));
   if (loading) return <p>Loading...</p>;
   if (err) return <p>Error!</p>;
@@ -20,7 +21,13 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <p>{data.recipe}</p>
+      {data.allLinks.map((link) => {
+        return (
+          <p key={link.id}>
+            {link.url} --- {link.slug}
+          </p>
+        );
+      })}
     </div>
   );
 }
